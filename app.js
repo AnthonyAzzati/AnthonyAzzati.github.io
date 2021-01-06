69d9d1cda45a400af03c1655c3e0983ab612665d
Anthony Azzati <anthonyazzati.dev@gmail.com>
2021-01-06 21:13:59 +0100

    MàJ de "app.js" suite erreur node server "cannot GET /"

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.static(__dirname + '/public'));
+
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use(bodyParser.json());
