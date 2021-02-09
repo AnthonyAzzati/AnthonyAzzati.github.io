5a6902d3a871118817a45e6c53759e26e3fcc46c
Anthony Azzati <anthonyazzati.dev@gmail.com>
2021-02-09 20:03:04 +0100

    Refactorisation de showProducts()

--- public/js/script.js
+++ public/js/script.js
@@ -10,7 +10,63 @@ menuBtn.addEventListener('click', () => {
 
 // Permet de récupérer les produits de l'API et de les afficher
 
+
 const showProducts = () => {
+    fetch('//localhost:3000/api/cameras')
+    .then(response => {
+            if (!response.ok) {
+                throw Error('Error: ', response.status)
+            } return response.json()
+        })
+    .then(data => {
+        product = data
+        getProducts()
+    })
+}
+
+
+const getProducts = () => {
+    let productDetails = document.getElementById('product-details')
+    productDetails.innerHTML +=
+    `
+    <figure class="flex flex-col rounded-md bg-white shadow-md m-4 p-4  md:flex-row md:mx-4">
+        <img src="${product.imageUrl}" class="w-80 h-auto object-cover rounded-md" alt="${product.name} ${product.description}">
+            <figcaption class="flex flex-col md:ml-4">
+                <h2 class="text-2xl pt-2 montserrat">
+                    ${product.name}
+                </h2>
+                <p class="py-2 montserrat lg:w-80">
+                    ${product.description}
+                </p>
+                <label for="Choisir un objectif" class="py-2" id="lenses">
+                </label>
+                <div class="flex flex-row justify-between items-center mt-2">
+                    <h3 class="text-xl font-medium merriweather">
+                        ${product.price/100}€
+                    </h3>
+                    <button class="ring-2 ring-black bg-platinum rounded-full p-2 text-sm font-semibold uppercase montserrat hover:bg-black hover:text-white focus:outline-none transition">
+                        Ajouter au panier
+                    </button>
+                </div>
+            </figcaption>
+    </figure>
+    `
+}
+
+/*const showLenses = () => {
+    for (let i = 0; i < product.lenses.length; i++) {
+        document.getElementById('lenses').innetHTML +=
+        `
+        <select name="objectifs">
+        <option value="${product.lenses[i]}" class="montserrat" selected>${product.lenses[i]}</option>
+        </select>
+        `
+    }
+} */
+
+showProducts()
+
+/* const showProducts = () => {
     fetch('//localhost:3000/api/cameras')
         .then(response => {
             if (!response.ok) {
@@ -18,7 +74,7 @@ const showProducts = () => {
             } return response.json()
         })
         .then(data => {
-            let productHtml = data
+            let product = data
             .map(product => {
                 return `
                 <figure class="flex flex-col rounded-md bg-white shadow-md m-4 p-4  md:flex-row md:mx-4">
@@ -46,7 +102,7 @@ const showProducts = () => {
             })
             .join('')
             let productDetails = document.getElementById('product-details')
-            productDetails.insertAdjacentHTML("afterbegin", productHtml)
+            productDetails.insertAdjacentHTML("afterbegin", product)
         })
         .catch(error => {
             console.log(error)
@@ -55,14 +111,4 @@ const showProducts = () => {
 
 showProducts();
 
-const showLenses = () => {
-    for (let i = 0; i < product.lenses.length; i++) {
-        document.getElementById('lenses').insertAdjacentHTML("afterbegin", 
-        `
-        <select name="objectifs">
-        <option value="${product.lenses[i]}" class="montserrat" selected>${product.lenses[i]}</option>
-        </select>
-        `
-        )
-    }
-}
+*/
