232dd9ddc67fd22f39c7e7ed2ad546491c342a2e
Anthony Azzati <anthonyazzati.dev@gmail.com>
2021-02-09 18:15:09 +0100

    Ajout d'une requête à l'API: récupère les produits en vente

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,22 @@
-let menuBtn = document.getElementById('menuBtn');
-let mobileMenu = document.getElementById('mobileMenu');
+let menuBtn     = document.getElementById('menuBtn')        // On récupère l'icône du menu sur smartphone
+let mobileMenu  = document.getElementById('mobileMenu')     // On récupère les liens du menu
 
-menuBtn.addEventListener('click', () => {
-    mobileMenu.classList.toggle('active');
-});
+// Lors du clic de l'utilisateur (sur smartphone), le menu apparaît et disparaît
+menuBtn.addEventListener('click', () => {                                       
+    mobileMenu.classList.toggle('active')
+})
+
+
+// Permet de récupérer les produits en vente sur le site
+const getProducts  = async () => {                                  
+    let response = await fetch('//localhost:3000/api/cameras')
+    if (response.ok) {
+        let data = await response.json()
+        console.log(data)
+    } else {
+        console.error('Erreur: ', response.status)
+    }
+}
+
+getProducts()                                                       
+
