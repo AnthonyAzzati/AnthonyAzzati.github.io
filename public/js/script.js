2e070402fc5d81f9016c1c8a7855a72da2e79bef
Anthony Azzati <anthonyazzati.dev@gmail.com>
2021-02-10 11:14:11 +0100

    MàJ de showProducts()

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+"use strict"
+
 let menuBtn     = document.getElementById('menuBtn')        // On récupère l'icône du menu sur smartphone
 let mobileMenu  = document.getElementById('mobileMenu')     // On récupère les liens du menu
 
@@ -10,7 +12,8 @@ menuBtn.addEventListener('click', () => {
 
 // Permet de récupérer les produits de l'API et de les afficher
 
-/*
+let product
+
 const showProducts = () => {
     fetch('//localhost:3000/api/cameras')
     .then(response => {
@@ -20,48 +23,94 @@ const showProducts = () => {
         })
     .then(data => {
         product = data
+        console.log(product)
         getProducts()
-        console.log(data)
+        showLenses()
     })
     .catch(error => {
         console.log(error)
     })
 }
 
-
 const getProducts = () => {
-    let productDetails = document.getElementById('product-details')
-    productDetails.innerHTML +=
-    `
-    <figure class="flex flex-col rounded-md bg-white shadow-md m-4 p-4  md:flex-row md:mx-4">
-        <img src="" class="w-80 h-auto object-cover rounded-md" alt="${product.name} ${product.description}">
+    for (let i = 0; i < product.length; i++) {
+        let productDetails = document.getElementById('product-details')
+        productDetails.innerHTML +=
+        `
+        <figure class="flex flex-col rounded-md bg-white shadow-md m-4 p-4  md:flex-row md:mx-4">
+        <img src="${product[i].imageUrl}" class="w-80 h-auto object-cover rounded-md" alt="${product[i].name} ${product[i].description}">
             <figcaption class="flex flex-col md:ml-4">
-                <h2 class="text-2xl pt-2 montserrat">
-                    ${product.name}
-                </h2>
-                <p class="py-2 montserrat lg:w-80">
-                    ${product.description}
-                </p>
-                <label for="Choisir un objectif" class="py-2" id="lenses">
-                </label>
-                <div class="flex flex-row justify-between items-center mt-2">
-                    <h3 class="text-xl font-medium merriweather">
-                        ${product.price/100}€
-                    </h3>
-                    <button class="ring-2 ring-black bg-platinum rounded-full p-2 text-sm font-semibold uppercase montserrat hover:bg-black hover:text-white focus:outline-none transition">
-                        Ajouter au panier
-                    </button>
-                </div>
+            <h2 class="text-2xl pt-2 montserrat">
+                ${product[i].name}
+            </h2>
+            <p class="py-2 montserrat lg:w-80">
+                ${product[i].description}
+            </p>
+            <label for="Choisir un objectif" class="py-2" id="lenses">
+            </label>
+            <div class="flex flex-row justify-between items-center mt-2">
+                <h3 class="text-xl font-medium merriweather">
+                    ${product[i].price/100}€
+                </h3>
+                <button class="ring-2 ring-black bg-platinum rounded-full p-2 text-sm font-semibold uppercase montserrat hover:bg-black hover:text-white focus:outline-none transition">
+                    Ajouter au panier
+                </button>
+            </div>
             </figcaption>
-    </figure>
-    `
+        </figure>
+        `
+    }
 }
 
+const showLenses = () => {
+    const lenses = document.getElementById('lenses')
+    for (let i = 0; i < product.lenses.length; i++) {
+        lenses.innerHTML +=
+        `
+        <select name="objectifs">
+            <option value="${product.lenses[i]}" class="montserrat" selected>
+                ${product.lenses[i]}
+            </option>
+        </select>
+        `
+    }
+}
+
+
 showProducts()
-*/
+/*
+const getProducts = () => {
+    const productDetails = document.getElementById('product-details')
+    for (let i = 0, i < product.length, i++) {
+        productDetails.innerHTML +=
+        `
+        <figure class="flex flex-col rounded-md bg-white shadow-md m-4 p-4  md:flex-row md:mx-4">
+        <img src="${product[i].Url}" class="w-80 h-auto object-cover rounded-md" alt="${product[i].name} ${product[i].description}">
+        <figcaption class="flex flex-col md:ml-4">
+        <h2 class="text-2xl pt-2 montserrat">
+        ${product[i].name}
+        </h2>
+        <p class="py-2 montserrat lg:w-80">
+        ${product[i].description}
+        </p>
+        <label for="Choisir un objectif" class="py-2" id="lenses">
+        </label>
+        <div class="flex flex-row justify-between items-center mt-2">
+        <h3 class="text-xl font-medium merriweather">
+        ${product[i].price/100}€
+        </h3>
+        <button class="ring-2 ring-black bg-platinum rounded-full p-2 text-sm font-semibold uppercase montserrat hover:bg-black hover:text-white focus:outline-none transition">
+        Ajouter au panier
+        </button>
+        </div>
+        </figcaption>
+        </figure>
+        `
+    } 
+} */
 
 
-
+/*
 const showProducts = () => {
     fetch('//localhost:3000/api/cameras')
     .then(response => {
@@ -106,19 +155,9 @@ const showProducts = () => {
 }
 
 showProducts();
+*/
 
 
 
 
 
-
-/* const showLenses = () => {
-    for (let i = 0; i < product.lenses.length; i++) {
-        document.getElementById('lenses').innerHTML +=
-        `
-        <select name="objectifs">
-        <option value="${product.lenses[i]}" class="montserrat" selected>${product.lenses[i]}</option>
-        </select>
-        `
-    }
-} */
