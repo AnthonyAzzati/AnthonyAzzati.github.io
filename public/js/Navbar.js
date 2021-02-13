9a382e9ab5ff9c0511a4badb1aa621157a731d02
Anthony Azzati <anthonyazzati.dev@gmail.com>
2021-02-13 12:47:45 +0100

    Création du fichier Navbar.js

---
+++ public/js/Navbar.js
@@ -0,0 +1,14 @@
+"use strict"
+
+class Navbar {
+    constructor() {
+        this.menuBtn = document.getElementById('menuBtn')
+        this.mobileMenu = document.getElementById('mobileMenu')
+    }
+
+    toggleMenu() {
+        this.menuBtn.addEventListener('click', () => {
+            this.mobileMenu.classList.toggle('active')
+        })
+    }
+}
