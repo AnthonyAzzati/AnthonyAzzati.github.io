3fa554a495eb95063fc557c18231e2831a066795
Anthony Azzati <anthonyazzati.dev@gmail.com>
2021-02-16 14:17:04 +0100

    Appel de la fonction "toggleDetails()"

--- public/js/script.js
+++ public/js/script.js
@@ -5,21 +5,4 @@ navbar.toggleMenu()
 
 let products = new Products()
 products.showProducts()
-
-/*
-
-const showLenses = () => {
-    for (let i = 0; i < product.lenses.length; i++) {
-        document.getElementById('lenses').innerHTML +=
-        `
-        <select name="objectifs">
-            <option value="${product.lenses[i]}" class="montserrat" selected>
-                ${product.lenses[i]}
-            </option>
-        </select>
-        `
-    }
-}
-
-showProducts()
-*/
+products.toggleDetails()
